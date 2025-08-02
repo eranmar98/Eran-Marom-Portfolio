d7927ac762758cd7106a6897b3333ffe307c02cc
eranmar98 <maromeran8@gmail.com>
2025-08-02 22:38:54 +0300

    Massive Improvements

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -6,14 +6,33 @@ export function isInViewport(el) {
 
 // Animate the progress bars once when in view
 export function animateSkillsOnScroll() {
-    
-    //get each skill card
-  document.querySelectorAll('.skill-card').forEach(card => {
+  //get each skill card
+  document.querySelectorAll('.skill-card').forEach((card) => {
     const progress = card.querySelector('.progress');
     const target = card.dataset.progress;
-     if (isInViewport(card) && !progress.classList.contains('animated')) {
+    if (isInViewport(card) && !progress.classList.contains('animated')) {
       progress.style.width = target;
       progress.classList.add('animated');
     }
   });
 }
+
+// Function to mark the link in the navbar when the same section is on display
+export function updateActiveLink() {
+    let currentSection = '';
+
+    // loop through <section> tags
+    sections.forEach(section => {
+      const rect = section.getBoundingClientRect();
+      if (rect.top <= 100 && rect.bottom >= 100) {
+        currentSection = section.id;
+      }
+    });
+
+    navLinks.forEach(link => {
+      link.classList.remove('active');
+      if (link.getAttribute('href') === `#${currentSection}`) {
+        link.classList.add('active');
+      }
+    });
+  }
--- Scripts/index.js
+++ Scripts/index.js
@@ -1,5 +1,17 @@
-import { isInViewport, animateSkillsOnScroll } from './functions.js';
+import { animateSkillsOnScroll } from './functions.js';
 
 // Listen for scroll and trigger check
 window.addEventListener('scroll', animateSkillsOnScroll);
 window.addEventListener('load', animateSkillsOnScroll); // if it's already in view
+
+// AOS library trigger 
+document.addEventListener('DOMContentLoaded', () => {
+  AOS.init({ duration: 1000, once: true });
+
+
+  const sections = document.querySelectorAll('section');
+  const navLinks = document.querySelectorAll('#nav-bar ul li a');
+
+  window.addEventListener('scroll', updateActiveLink);
+  updateActiveLink();
+});
